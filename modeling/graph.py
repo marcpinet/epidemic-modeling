9676a0cd3dc77e1a4d2f506e9d6d609408470084
Marc Pinet <marc.pinet.06@gmail.com>
2021-12-15 19:15:22 +0100

    Added death recap to end plot

--- modeling/graph.py
+++ modeling/graph.py
@@ -64,6 +64,10 @@ def create(file):
     plt.xlabel("Time (days)")
     plt.ylabel("SEIDR Populations")
     plt.legend(loc="right")
+    x = time[round((len(time) - 1) / 2)]
+    pop = susceptible[-1] + infected[-1] + recovered[-1] + exposed[-1] + dead[-1]
+    y = pop - (pop/10)
+    plt.text(x, y, "Deadly toll: " + str(dead[-1]) + " deaths")
     plt.pause(0.001)
 
     # Show plots
