6e95df74a05aeb565cfd815f4ff0f5db2cdc9e4d
Marc <marc.pinet.06@gmail.com>
2021-12-15 17:46:45 +0100

    Delete release_creator.bat

--- release_creator.bat
+++
@@ -1,7 +0,0 @@
-copy main.py release /Y
-mkdir release\modeling
-copy modeling\* release\modeling\ /Y
-copy README.md release /Y
-copy requirements.txt release /Y
-copy LICENSE release /Y
-copy .gitignore release /Y
